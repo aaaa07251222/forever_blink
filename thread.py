9c69ca9c02711334b41e34d5fb22d315579f2a05
aaaa07251222 <79737486+aaaa07251222@users.noreply.github.com>
2021-05-08 15:49:20 +0800

    Update thread.py

--- thread.py
+++ thread.py
@@ -3,35 +3,58 @@ from flask import Flask
 from flask import request
 import threading
 import time
+global var
+app = Flask(__name__)
 
+@app.route('/',methods = ['GET'])
 
 
 def job():
-    my_led_status = request.args.get('value')
+    global var
+    my_led_status = request.args.get('/var')
     while 7==7:
-            GPIO.output(25, GPIO.HIGH)
-            time.sleep(1)
-            GPIO.output(25, GPIO.LOW)
-            time.sleep(1)
-    return 'LED_string'
-
-t = threading.Thread(target = job)
-
+      if var == 2:
+          break
+      GPIO.output(25, GPIO.HIGH)
+      time.sleep(1)
+      GPIO.output(25, GPIO.LOW)
+      time.sleep(1)
 
 def start():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(25, GPIO.OUT)
-    GPIO.output(25, GPIO.HIGH)
-    return 'LED_ON'
-
-if __name__== '__main__':
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(25, GPIO.OUT)
-
-
+    global var
+    my_led_status = request.args.get('/var')
+    if my_led_status == "0":
+       GPIO.output(25, GPIO.LOW)
+       return 'LED_OFF'
+       var = 2
+    elif my_led_status == "1":
+       GPIO.output(25, GPIO.HIGH)
+       return 'LED_ON'
+       var = 2
+    elif my_led_status == "2":
+        t = threading.Thread(target = job)
+        t.start()
+        var = 1
+        return "start"
+    elif my_led_status == "3":
+        var = 2
+        return "stop"
+    t.start()
+    t.join()
 for i in range(3):
     print("Main thread:", i)
     time.sleep(1)
 
-t.join()
+
 print("Done.")
+
+if __name__== '__main__':
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(25, GPIO.OUT)
+    app.run()
+    global var
+    var = 1
+    print(var)
+    func()
+    print(var)
+
